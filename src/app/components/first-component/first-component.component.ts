c0b65af21c061d71419c5e6aecc337733c855d34
Francisco Alves Menezs <moedas.com.br@Mac-mini-de-Francisco.local>
2023-11-10 18:56:57 -0300

    Aula #6

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FirstComponentComponent implements OnInit {
   name: string = "Chicão"
   age : number = 18
-  ocupation: string = "Dev"
+  job: string = "Dev"
   Isadmin: boolean = true 
   
   
